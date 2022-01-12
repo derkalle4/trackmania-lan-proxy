3e4cbf53f22832b717a67bc43db23fb723854304
Karl-Martin Minkner <kalle@kandru.de>
2022-01-12 19:44:57 +0100

    optimize build

--- setup.py
+++ setup.py
@@ -1,5 +1,16 @@
 from distutils.core import setup
+from datetime import datetime
 import py2exe
 
 
-setup(console=['proxy.py'])
+setup(
+    options = {'py2exe': {'bundle_files': 1, 'compressed': True}},
+    console = [
+        {
+            'script':'proxy.py',
+            'icon_resources':[(0,'icon.ico')],
+            'dest_base' : 'trackmania_proxy_{}'.format(datetime.now().strftime("%d_%m_%Y_%H_%M_%S"))
+        }
+    ],
+    zipfile = None,
+)
